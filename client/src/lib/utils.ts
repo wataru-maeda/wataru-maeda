e238e3368cb058198da8e1d02883c149e071d923
Wataru Maeda <w.maeda.jp@gmail.com>
2026-01-13 22:45:39 -0800

    Remove project files

--- client/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-
-/**
- * Utility function for merging class names.
- * Note: With Chakra UI, prefer using Chakra's style props over className.
- */
-export function cn(...inputs: ClassValue[]) {
-  return clsx(inputs);
-}
--- client/src/pages/Home/_sections/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { Text } from "@chakra-ui/react";
-
-export default function FooterSection() {
-  return (
-    <Text fontSize="sm" color="fg.muted" textAlign="center" py={6}>
-      © {new Date().getFullYear()} Wataru Maeda. All rights reserved.
-    </Text>
-  );
-}
